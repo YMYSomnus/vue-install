8c8c09d112fade07992ab15abaa28f5adcaf18fa
yangmingyue <yangmingyue@qianshengqian.com>
2018-08-03 11:24:34 +0800

    add vue-install

---
+++ src/lib/index.js
@@ -0,0 +1,11 @@
+import vueInstall from "./vue-install.vue"
+const vueFirstInstall = {
+  install(vue, option) {
+    Vue.component(vueInstall.name, vueInstall)
+  }
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(paykeyboard);
+}
+export default vueFirstInstall
