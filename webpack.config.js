6e08bd8694d42f8eb68e0aea58b90ae21c4ec97f
yangmingyue <yangmingyue@qianshengqian.com>
2018-08-03 16:55:58 +0800

    项目名字修改

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     // filename: 'build.js', // 打包后输出的文件名
-    filename: 'VueInstall.js', // 我们可不想打包后叫build 多low啊 起一个与项目相对应的
-    library: 'vueFirstInstall', // library指定的就是你使用require时的模块名，这里便是require("PayKeyboard")
+    filename: 'vue-install.js', // 我们可不想打包后叫build 多low啊 起一个与项目相对应的
+    library: 'vue-install', // library指定的就是你使用require时的模块名，这里便是require("PayKeyboard")
     libraryTarget: 'umd', //libraryTarget会生成不同umd的代码，例如可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
     umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
   },
